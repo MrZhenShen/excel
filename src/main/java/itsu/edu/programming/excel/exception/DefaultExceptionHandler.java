ebf8230281cc041bc3c013bf1967354f144403fb
mrzhenshen <mrzhenshen@gmail.com>
2023-11-24 13:52:17 +0200

    create ExceptionResponse, WebException, DefaultExceptionHandler

---
+++ src/main/java/itsu/edu/programming/excel/exception/DefaultExceptionHandler.java
@@ -0,0 +1,13 @@
+package itsu.edu.programming.excel.exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class DefaultExceptionHandler {
+    @ExceptionHandler(WebException.class)
+    public ResponseEntity<ExceptionResponse> webException(WebException exception) {
+        return exception.toExceptionResponse();
+    }
+}
